f969ed3002c0edacae18bd485b430e469364ff31
Alex <Alex@DESKTOP-LD4FGDN>
2019-04-10 17:50:22 -0500

    Add project files.

---
+++ ModernUINavigationApp1/Pages/BasicPage1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+namespace ModernUINavigationApp1.Pages
+{
+    /// <summary>
+    /// Interaction logic for BasicPage1.xaml
+    /// </summary>
+    public partial class BasicPage1 : UserControl
+    {
+        public BasicPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String sql = "Select * from Player";
+            DataAccess.ExecuteSQL(sql);
+            DataTable dt = DataAccess.GetDataTable(sql);
+            // DataView dataView = new DataView(dt);
+            list.DataContext = dt.DefaultView;
+        }
+    }
+}
