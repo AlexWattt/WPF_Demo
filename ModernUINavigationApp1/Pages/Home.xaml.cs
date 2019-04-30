48dfff2918030ea2ec25d4b36dea1de337609080
alex <alex@DESKTOP-HHK9RFH>
2019-04-30 16:35:12 -0500

    I am DONE with my part

--- ModernUINavigationApp1/Pages/Home.xaml.cs
+++ ModernUINavigationApp1/Pages/Home.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Diagnostics;
 
 namespace ModernUINavigationApp1.Pages
 {
@@ -23,6 +25,19 @@ namespace ModernUINavigationApp1.Pages
         public Home()
         {
             InitializeComponent();
+            String sql = "SELECT Country.NAME AS CountryName, League.name AS LeagueName	FROM Country	JOIN League ON League.country_id = Country.id;";
+            // change visiabilty of the two tables since I will be going back and forth through them.
+            list.Visibility = Visibility.Visible;
+            DataAccess.ExecuteSQL(sql);
+            DataTable dt = DataAccess.GetDataTable(sql);
+
+
+            list.DataContext = dt.DefaultView;
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            
+
         }
     }
 }
--- ModernUINavigationApp1/Pages/TeamBets.xaml.cs
+++ ModernUINavigationApp1/Pages/TeamBets.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Data;
 namespace ModernUINavigationApp1.Pages
 {
     /// <summary>
@@ -23,6 +23,14 @@ namespace ModernUINavigationApp1.Pages
         public BasicPage2()
         {
             InitializeComponent();
+            String sql = "SELECT team_long_name, team_short_name,buildUpPlaySpeed,buildUpPlaySpeedClass,buildUpPlayDribbling,buildUpPlayDribblingClass,buildUpPlayPassing,buildUpPlayPassingClass,buildUpPlayPositioningClass,chanceCreationPassing,chanceCreationPassingClass,chanceCreationCrossing,chanceCreationCrossingClass,chanceCreationShooting,chanceCreationShootingClass,chanceCreationPositioningClass,defencePressure,defencePressureClass,defenceAggression,defenceAggressionClass,defenceTeamWidth,defenceTeamWidthClass,defenceDefenderLineClass	FROM TEAM 	JOIN Team_Attributes ON Team.team_api_id = Team_Attributes.team_api_id;";
+            // change visiabilty of the two tables since I will be going back and forth through them.
+            list.Visibility = Visibility.Visible;
+            DataAccess.ExecuteSQL(sql);
+            DataTable dt = DataAccess.GetDataTable(sql);
+            
+
+            list.DataContext = dt.DefaultView;
         }
     }
 }
